6e54c37a76456319316e38c38e63e4a60ef60a59
Zenek <35767830+Zenek-Hastro@users.noreply.github.com>
2020-10-26 20:33:24 +0100

    Released version 1.2b (check changelogs)+ README

---
+++ README.md
@@ -0,0 +1,58 @@
+# Instadownloader
+A simple downloader in Python!
+
+## Index
+- [Features](#features)
+- [Download](#download)
+- [About](#about)
+- [Change Log](#servizi-esterni-utlizzati)
+
+-----------
+### Features
+- [x] Download any images-account
+- [x] Download any videos-account
+- [x] Log-in System for download Private Accounts
+- [x] Clear System, for correctly clear the metadata
+
+-----------
+### Download
+Name | Type | Description
+--- | --- | ---
+`Main.py` | *PythonFile* | Source Code
+`Main.exe` | *ExecutableFile* | 32-bit Program
+
+-----------
+### About
+ Type | Description
+--- | --- | 
+`Developer` | *Zenek* 
+`First Release` | *v1.0b* 
+`Current Release` | *v1.2b* 
+
+-----------
+### ChangeLog
+
+### Version 1.0b
+
+- [x] Initial Commit
+- [x] You can Download photo (public account only)
+- [x] You cana Downlaod video (public account only)
+- [x] Clear System, for correctly clear the metadata
+
+### Version 1.1b
+
+- [x] Clear System, for correctly clear the metadata
+- [x] Login System (download private content from private account!)
+- [x] Unnecessary libs removed, code clear, new comments
+- [x] Minor bug fix
+
+### Version 1.2b
+
+- [x] Log-in system now works correctly
+- [x] Clear system now target `Highlights`, `Stories`, `Igtv`, `Tags`, `Updates` and correctly remove unnecessary files
+- [x] Auto-Login, save your session (you don't have to re-enter your account all times)
+- [x] Log-in system now support Multi-Accounts!
+- [x] Added Update system for check the lastest media on all accounts collected
+- [x] Bug fix and Stability Improved
+
+
--- main.py
+++ main.py
@@ -31,6 +31,7 @@ from pathlib import Path                                                        
 # Definition & Declaration                                                                                                                                                      #
 def clear(): os.system('cls') #Clear Console                                                                                                                                    #
 def start(): os.system('start') #Open Console                                                                                                                                   #
+                                                                                                                                                                                #
 # ===============================================================================================================================================================================
 #                                                                                                                                                                               #
 #                                                                       M A I N  -  F U N C T I O N                                                                             #
@@ -39,37 +40,106 @@ def start(): os.system('start') #Open Console                                   
 
 # Login System
 clear()
+user_login_status = 0  # check the log-in status
+list_of_lists = [] # check users
+
 IGlogin = input('Would you like login on instagram (y/n)? ').lower()
-if IGlogin.startswith('y'):
+
+if (os.path.isfile("users.zk") and IGlogin.startswith('y')):
+    LoadSession = input('We have found a session already opened! Would you like load it (y/n)?').lower()
+    if LoadSession.startswith('y'):
+        clear()
+        f = open("users.zk", "r")
+        account = f.read()
+        user_login_status = 1
+        os.system(f"instaloader --login {account}")
+        
+if (IGlogin.startswith('y') and user_login_status == 0):
     clear()
     account = input('Enter your instagram account> ')
+    user_login_status = 1
     os.system(f"instaloader --login {account}")
+    # Save currently session (only nickname, NO PASSWORD!)
+    file_object = open("users.zk", "w")
+    file_object.write(account)
+    file_object.close
 
 elif IGlogin.startswith('n'):
     clear()
-    print(Fore.RED+"\t!IF YOU DOESN'T LOGIN, YOU COULD NOT DOWNLOAD ANY PRIVATE ACCOUNTS!\n\n")
+    user_login_status = 0
+    print(Fore.RED+"\t !!IF YOU DOESN'T LOGIN, YOU COULD NOT DOWNLOAD STORIES, HIGHLIGHTS AND PRIVATE ACCOUNT !!\n\n")
 
-else:
-    clear()
-    print("\n\nERROR: YOU MUST TYPE (y)/(n)!\n\n")
-    exit()
+
+# Check For Updates 
+users_update = input('Would you like check profiles updates from collected account (y/n)? ').lower()
+
+if (users_update.startswith('y')):
+    with open('account.zk', 'r') as f:
+        account_list = [line.strip() for line in f]
+
+    # Get list lenght
+    length = len(account_list) 
     
+    # Iterate in to the list 
+    for i in range(length): 
+        os.system(f"instaloader --fast-update --login={account} {account_list[i]}")
+
+        # Clear Metadata 
+        for root, dirs, files in os.walk(account_list[i]):
+            for file in files:
+                if file.endswith(".txt") or file.endswith(".xz"):
+                    #print(os.path.join(root, file)) No need to print all files deleted. Uncomment for check it !
+                    os.remove(os.path.join(root, file)) 
+
+        print("\n\nUpdated: " +account_list[i]+ "\n")
+        time.sleep(3) 
+
+
+
 
 # Start Downloading System
+clear()
 name = input(Fore.WHITE+"Enter Profile Name To Download: ")
-os.system(f"instaloader {name}")
+
+# Save Account (used for check the updates!)
+with open('account.zk', 'r') as f:
+    account_list = [line.strip() for line in f]
+
+if(name in account_list):
+  f.close
+  print("\nALREADY ON THIS LIST!\n")
+  time.sleep(5)
+else:
+    f.close
+    file_object = open("account.zk", "a")
+    file_object.write(name + "\n")
+    file_object.close
+    print("\nAdded " +name+" to account.zk!\n")
+
+
+if (user_login_status == 0):
+    os.system(f"instaloader {name} ") # CALL WITHOUT LOGIN (NO PARAMETERS, ONLY MAIN PICTURES PROFILE)
+
+elif (user_login_status == 1):
+
+    os.system(f"instaloader --stories --highlights --tagged --igtv --login={account} {name}") # MAIN CALL WITH ALL PARAMETERS
+
+else:
+    print("CRITICAL ERROR")
+    exit()
 
 
 # Clear Console 
 print("\n\nClearing Metadata & Information...\n")
-time.sleep(5)
+time.sleep(3)
 
 
-# Clear Metadata
-mydir = Path("""""", name)
-filelist = [f for f in os.listdir(mydir) if f.endswith(".txt") or f.endswith(".xz")]
-for f in filelist:
-    os.remove(os.path.join(mydir, f))
+# Clear Metadata 
+for root, dirs, files in os.walk(name):
+    for file in files:
+        if file.endswith(".txt") or file.endswith(".xz"):
+            #print(os.path.join(root, file)) No need to print all files deleted. Uncomment for check it !
+            os.remove(os.path.join(root, file)) 
 
 
 # Opening Downloaded Folder
